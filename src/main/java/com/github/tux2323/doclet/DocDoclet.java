1a392c5f6f57a94e86dbbe8d22a64912dd5b6340
Elek, Marton <level2@anzix.net>
2013-08-17 18:42:59 +0200

    reformating code files

--- src/main/java/com/github/tux2323/doclet/DocDoclet.java
+++ src/main/java/com/github/tux2323/doclet/DocDoclet.java
@@ -10,62 +10,62 @@ import com.thoughtworks.xstream.XStream;
 public class DocDoclet {
 
     private static final String DOCU_XML_FILE = "docu.xml";
-    
+
     private static final String JAVADOC_DESCRIPTION_TAG = "description";
     private static final String JAVADOC_TITLE_TAG = "title";
-    
+
     private final File reportDirectory = new File("target/test-docu/");
-    
+
     private final XStream xStream = new XStream();
-    
+
     public static boolean start(RootDoc root) throws Exception {
-	new DocDoclet().createTestDocumentation(root.classes());
-	return true;
+        new DocDoclet().createTestDocumentation(root.classes());
+        return true;
     }
-    
+
     protected final void createTestDocumentation(ClassDoc[] classes) throws Exception {
         final Documentation documentation = new Documentation();
         for (ClassDoc classDoc : classes) {
-	    final TestClassDoc testClassDoc = new TestClassDoc(classDoc.name());
+            final TestClassDoc testClassDoc = new TestClassDoc(classDoc.name());
             documentation.addTestClassDoc(testClassDoc);
-	    for (MethodDoc methodDoc : classDoc.methods()) {
-		TestDoc testDoc = new TestDoc(methodDoc.name());
-	        testClassDoc.addTestDoc(testDoc);
-		testDoc.setTestTitle(extractTitle(methodDoc));
-		testDoc.setDescription(extractDescription(methodDoc));
-	    }
+            for (MethodDoc methodDoc : classDoc.methods()) {
+                TestDoc testDoc = new TestDoc(methodDoc.name());
+                testClassDoc.addTestDoc(testDoc);
+                testDoc.setTestTitle(extractTitle(methodDoc));
+                testDoc.setDescription(extractDescription(methodDoc));
+            }
         }
-	writeXmlModel(documentation);
+        writeXmlModel(documentation);
     }
 
     private String extractDescription(MethodDoc methodDoc) {
-	return extractTag(methodDoc, JAVADOC_DESCRIPTION_TAG);
+        return extractTag(methodDoc, JAVADOC_DESCRIPTION_TAG);
     }
 
     private String extractTitle(MethodDoc methodDoc) {
-	return extractTag(methodDoc, JAVADOC_TITLE_TAG);
+        return extractTag(methodDoc, JAVADOC_TITLE_TAG);
     }
-    
-    private String extractTag(MethodDoc methodDoc, String tagName){
-	String tagTextContent = null;
-	final Tag[] tags = methodDoc.tags(tagName);
-	if (tags.length > 0) {
-	  tagTextContent = tags[0].text();
-	}
-	return tagTextContent;
+
+    private String extractTag(MethodDoc methodDoc, String tagName) {
+        String tagTextContent = null;
+        final Tag[] tags = methodDoc.tags(tagName);
+        if (tags.length > 0) {
+            tagTextContent = tags[0].text();
+        }
+        return tagTextContent;
     }
 
     private void writeXmlModel(Documentation documentation) throws FileNotFoundException {
-	getReportDirectory().mkdirs();
-	xStream.toXML(documentation, new FileOutputStream(getDocumentationFile()));
+        getReportDirectory().mkdirs();
+        xStream.toXML(documentation, new FileOutputStream(getDocumentationFile()));
     }
 
     public File getDocumentationFile() {
-	return new File(getReportDirectory(), DOCU_XML_FILE);
+        return new File(getReportDirectory(), DOCU_XML_FILE);
     }
 
     public File getReportDirectory() {
-	return reportDirectory;
+        return reportDirectory;
     }
 
 }
--- src/main/java/com/github/tux2323/doclet/Documentation.java
+++ src/main/java/com/github/tux2323/doclet/Documentation.java
@@ -6,45 +6,45 @@ import java.util.List;
 public class Documentation {
 
     private final List<TestClassDoc> testClassDocs = new LinkedList<TestClassDoc>();
-    
-    public void addTestClassDoc(TestClassDoc testClassDoc){
-	getTestClassDocs().add(testClassDoc);
+
+    public void addTestClassDoc(TestClassDoc testClassDoc) {
+        getTestClassDocs().add(testClassDoc);
     }
 
     public List<TestClassDoc> getTestClassDocs() {
-	return testClassDocs;
+        return testClassDocs;
     }
 
     @Override
     public String toString() {
-	return "Documentation [testClassDocs=" + testClassDocs + "]";
+        return "Documentation [testClassDocs=" + testClassDocs + "]";
     }
-    
+
     @Override
     public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result
-		+ ((testClassDocs == null) ? 0 : testClassDocs.hashCode());
-	return result;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((testClassDocs == null) ? 0 : testClassDocs.hashCode());
+        return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	Documentation other = (Documentation) obj;
-	if (testClassDocs == null) {
-	    if (other.testClassDocs != null)
-		return false;
-	} else if (!testClassDocs.equals(other.testClassDocs))
-	    return false;
-	return true;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Documentation other = (Documentation) obj;
+        if (testClassDocs == null) {
+            if (other.testClassDocs != null)
+                return false;
+        } else if (!testClassDocs.equals(other.testClassDocs))
+            return false;
+        return true;
     }
-    
-    
+
+
 }
